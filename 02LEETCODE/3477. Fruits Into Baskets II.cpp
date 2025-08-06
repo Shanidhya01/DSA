2693c246eae71d702ae4a9ba13acd72f0d843ab2
Shanidhya01 <luckykumar0011s@gmail.com>
2025-08-06 09:20:28 +0530

    3477. Fruits Into Baskets II

---
+++ 02LEETCODE/3477. Fruits Into Baskets II.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        const int n=fruits.size();
+        int ans=n;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if (fruits[i]<=baskets[j]){
+                    ans--;
+                    baskets[j]=0;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
