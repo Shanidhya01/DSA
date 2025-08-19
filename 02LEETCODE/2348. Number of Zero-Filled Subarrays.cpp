aed8b667808c9afe6cd9104019665e4da08678e1
Shanidhya01 <luckykumar0011s@gmail.com>
2025-08-19 22:34:14 +0530

    2348. Number of Zero-Filled Subarrays

---
+++ 02LEETCODE/2348. Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long cnt = 0, streak = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                streak++;
+                cnt += streak;
+            } else {
+                streak = 0;
+            }
+        }
+        return cnt;
+    }
+};
