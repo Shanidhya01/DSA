48b8a26a841f95c2c2ddf780ae6cf610904df382
Shanidhya01 <luckykumar0011s@gmail.com>
2025-09-11 23:25:10 +0530

    1002. Find Common Characters  Merge two sorted linked lists  Reverse a linked list  12. Integer to Roman  98. Validate Binary Search Tree

---
+++ 03GFG/LINKED LIST/Merge two sorted linked lists.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node {
+ public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+  public:
+    Node* sortedMerge(Node* head1, Node* head2) {
+        Node dummy(0); 
+        Node* tail = &dummy;
+        while (head1 && head2) {
+            if (head1->data <= head2->data) {
+                tail->next = head1;
+                head1 = head1->next;
+            } else {
+                tail->next = head2;
+                head2 = head2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = head1 ? head1 : head2;
+        return dummy.next;
+    }
+};
