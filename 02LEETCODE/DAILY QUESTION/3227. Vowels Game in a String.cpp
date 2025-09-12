319048eb9ccec9585732d289e7fdb81c11079bfa
Shanidhya01 <luckykumar0011s@gmail.com>
2025-09-12 15:24:09 +0530

    Added 131. Palindrome Partitioning 263. Ugly Number 3227. Vowels Game in a String

---
+++ 02LEETCODE/DAILY QUESTION/3227. Vowels Game in a String.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for(int i = 0; i < s.length(); i++){
+            char ch = s[i];
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                return true;
+            }
+        }
+        return false;
+    }
+};
