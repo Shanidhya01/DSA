a72a79ca5092799dc9878575cca68e0b7b819021
Shanidhya01 <luckykumar0011s@gmail.com>
2025-09-16 16:01:28 +0530

    1503. Last Moment Before All Ants Fall Out of a Plank

---
+++ 02LEETCODE/DAILY QUESTION/1503. Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int ans = 0;
+        for (int num : left) {
+            ans = max(ans, num);
+        }
+        
+        for (int num : right) {
+            ans = max(ans, n - num);
+        }
+        
+        return ans;
+    }
+};
