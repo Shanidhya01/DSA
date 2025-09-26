66368494a69902079dee5f899de3735c027e7794
Shanidhya01 <luckykumar0011s@gmail.com>
2025-09-26 12:38:55 +0530

    Search in a sorted Matrix

---
+++ 03GFG 160 DAYS/Matrix/Search in a sorted Matrix.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int n = mat.size(), m = mat[0].size();
+
+        int lo = 0, hi = n * m - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            int row = mid / m;
+            int col = mid % m;
+            if (mat[row][col] == x) 
+                return true;
+            if (mat[row][col] < x) 
+                lo = mid + 1;
+            else 
+                hi = mid - 1;
+        }
+        return false;
+    }
+};
