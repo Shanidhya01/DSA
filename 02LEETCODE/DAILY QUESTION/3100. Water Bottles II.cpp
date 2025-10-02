2b695a1427259e638326754574de73ba77a2e64f
Shanidhya01 <luckykumar0011s@gmail.com>
2025-10-02 14:27:48 +0530

    3100. Water Bottles II

---
+++ 02LEETCODE/DAILY QUESTION/3100. Water Bottles II.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int bottleDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange;
+            numExchange++;
+            bottleDrunk++;
+            emptyBottles++;
+        }
+        return bottleDrunk;
+    }
+};
