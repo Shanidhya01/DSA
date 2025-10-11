c98861df83a183a9cdd462b4472fcd2ad02591ec
Shanidhya01 <luckykumar0011s@gmail.com>
2025-10-11 21:13:35 +0530

    914. X of a Kind in a Deck of Cards

---
+++ 02LEETCODE/Counting/914. X of a Kind in a Deck of Cards.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        unordered_map<int, int>m;    
+        for(int d : deck)
+            m[d]++;   
+        int gcd = m[deck[0]];
+        for(auto i : m)
+            gcd = __gcd(gcd, i.second);    
+        return gcd != 1;    
+    }
+};
