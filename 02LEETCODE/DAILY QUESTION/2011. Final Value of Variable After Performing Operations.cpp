303884d9fdd0dcca85ea5fbcba3c1469086cdebe
Shanidhya01 <luckykumar0011s@gmail.com>
2025-10-20 12:03:23 +0530

    2011. Final Value of Variable After Performing Operations

---
+++ 02LEETCODE/DAILY QUESTION/2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+    int X = 0;
+        for (string& operation : operations) {
+            if (operation == "++X" || operation == "X++") {
+                X++; 
+            }
+            else {
+                X--; 
+            }
+        }
+        return X; 
+    }
+};
