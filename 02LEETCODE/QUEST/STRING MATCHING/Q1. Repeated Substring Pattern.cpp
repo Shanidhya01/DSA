e3544283ce792d345e8901340bf94b9a5facbc74
Shanidhya01 <luckykumar0011s@gmail.com>
2026-03-07 10:57:56 +0530

    Added STring MAtching from Quest

---
+++ 02LEETCODE/QUEST/STRING MATCHING/Q1. Repeated Substring Pattern.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+        for (int len = 1; len <= n/2; len++) {
+            if (n % len == 0) {
+                string sub = s.substr(0, len);
+                string built = "";
+                for (int i = 0; i < n/len; ++i) built += sub;
+                if (built == s) return true;
+            }
+        }
+        return false;
+    }
+};
